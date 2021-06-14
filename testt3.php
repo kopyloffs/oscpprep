5795a9beeab19a9af601a9ccfcde4c2678e79563
kopyloffs <kopyloff.s@gmail.com>
2021-06-14 12:17:26 +0300

    testionasd

--- testt3.php
+++ testt3.php
@@ -1,19 +1,8 @@
 <?php
-
-        $num = 1;
-        echo $num;
-
-    echo '<input type="button"
-           name="lol" 
-           value="Click to increment"
-           onclick="Inc()" />
-    <br>
-    <script>
-        function Inc()
-        {';
-            $num = 2;
-            echo $num;
-
-        echo '}
-    </script>';
+$num = 1;
+echo $num;
+echo '<input type="button" name="lol" value="Click to increment" onclick="Inc()" /> <br> <script>function Inc(){';
+$num = 2;
+echo $num;
+echo '}</script>';
 ?>
