b0f305e058571a1bec4e64c70c380fc452ff1faf
kopyloffs <kopyloff.s@gmail.com>
2021-06-14 12:20:02 +0300

    Delete testt3.php

--- testt3.php
+++
@@ -1,8 +0,0 @@
-<?php
-$num = 1;
-echo $num;
-echo '<input type="button" name="lol" value="Click to increment" onclick="Inc()" /> <br> <script>function Inc(){';
-$num = 2;
-echo $num;
-echo '}</script>';
-?>
